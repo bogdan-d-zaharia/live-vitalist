a227bf7a7226a704006a3117fc7c35cc79d09e2b
Bogdan <bogdanzaharia904@yahoo.com>
2026-07-05 17:58:29 +0300

    feat: am inlocuit meals journal cu un buton de add

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:live_vitalist/aliment/aliment.dart';
+import 'package:live_vitalist/aliment_editor/instance_editor.dart';
+import 'package:live_vitalist/custom_card.dart';
+import 'package:live_vitalist/day/day.dart';
+import 'package:live_vitalist/day/day_provider.dart';
+import 'package:live_vitalist/meals_journal.dart';
 
-import 'meals_journal.dart';
+// import 'meals_journal.dart';
 import 'nutrient_display.dart';
 import 'ratio_bars.dart';
 import 'settings.dart';
 import 'week_calendar.dart';
+
+class MealAddSelector extends ConsumerWidget {
+  const MealAddSelector({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final day = ref.watch(cachedSelectedDaysProvider).firstOrNull ?? Day();
+    final date = ref.watch(selectedDatesProvider).first;
+    final children = day.meals
+        .map(
+          (meal) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 120.0,
+              height: 40.0,
+              child: ElevatedButton(
+                child: Center(child: Text(meal.name)),
+                onPressed: () async {
+                  //TODO: Copied to `MealEditor`
+
+                  Navigator.pop(context);
+                  final newAliment = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => InstanceEditor(
+                        initialAliment: InstancedAliment.empty,
+                      ),
+                    ),
+                  );
+
+                  if (newAliment != null && newAliment.alimentID != '') {
+                    meal.aliments.add(newAliment);
+                    ref.read(dayCacheProvider.notifier).setDay(date, day);
+                  }
+                },
+                onLongPress: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => MealEditor(
+                        mealName: meal.name,
+                        date: date,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        )
+        .toList();
+
+    return MiniCard(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(children: children),
+        ),
+      ),
+    );
+  }
+}
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -23,6 +92,17 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_rounded),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => Center(
+              child: MealAddSelector(),
+            ),
+          );
+        },
+      ),
       appBar: AppBar(
         title: Text('Live Vitalist'),
         actions: [
@@ -61,7 +141,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           children: [
             WeekCalendar(),
             ConsumerRatioBars(),
-            MealsJournal(),
+            // MealsJournal(),
             NutrientDisplay(),
             SizedBox(height: 12.0),
           ],
